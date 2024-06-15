238094557022fb33fd6285affffa9b6c20274749
oscarperdana <sariminapp@gmail.com>
2024-06-15 09:31:37 +0700

    Initial Commit

---
+++ moviedb/moviedbApp.swift
@@ -0,0 +1,20 @@
+//
+//  moviedbApp.swift
+//  moviedb
+//
+//  Created by oscar perdana on 15/06/24.
+//
+
+import SwiftUI
+
+@main
+struct moviedbApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
